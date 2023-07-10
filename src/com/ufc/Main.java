70c76dd3fae1326691b3bccbafb8dc057453e4ed
Guilherme <guilhermebscontact@gmail.com>
2023-07-09 21:22:54 -0300

    Teste desserializacao

--- src/com/ufc/Main.java
+++ src/com/ufc/Main.java
@@ -2,6 +2,7 @@ package com.ufc;
 
 import com.ufc.usuario.Aluno;
 import com.ufc.usuario.Funcionario;
+import com.ufc.usuario.UsuarioAbstrato;
 import com.ufc.usuario.excecao.VNException;
 import com.ufc.usuario.repositorio.RepositorioUsuario;
 import com.ufc.usuario.repositorio.excecao.UJCException;
@@ -10,6 +11,8 @@ import com.ufc.usuario.repositorio.excecao.UNCException;
 public class Main {
 
     public static void main(String[] args) throws VNException, UJCException, UNCException {
-
+        RepositorioUsuario repo = new RepositorioUsuario();
+        Aluno aluno = new Aluno("null", "null", "null", "null", "null", "null");
+        repo.cadastrarAluno(aluno);
     }
 }
